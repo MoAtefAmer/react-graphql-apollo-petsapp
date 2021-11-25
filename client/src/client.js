0a672b146ce16dfa06d4956ebbf46d4f3755fb3a
MoAtefAmer <mo.atefamer@gmail.com>
2021-11-25 16:22:26 +0200

    1.0

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,68 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
-
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
+import { setContext } from 'apollo-link-context';
+import { ApolloLink } from 'apollo-link';
 /**
  * Create a new apollo client and export as default
  */
+
+const typeDefs = gql`
+  extend type User {
+    age: Int
+  }
+
+  extend type Pet {
+    vaccinated: Boolean!
+  }
+`;
+
+const resolvers = {
+  User: {
+    age() {
+      return 35;
+    },
+  },
+  Pet: {
+    vaccinated() {
+      return true;
+    },
+  },
+};
+
+const delay = setContext(
+  (request) =>
+    new Promise((success, fail) => {
+      setTimeout(() => {
+        success();
+      }, 800);
+    })
+);
+
+const http = new HttpLink({ uri: 'http://localhost:4000/' });
+const link = ApolloLink.from([delay, http]);
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache,
+  resolvers,
+  typeDefs,
+});
+
+// const query = gql`{
+//     characters{
+//         results{
+//             name,
+//             id
+//         }
+//     }
+// }`
+
+//  client.query({query}).then((result)=>{
+//      console.log(result)
+//  })
+
+export default client;
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,39 +1,111 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
-import Loader from '../components/Loader'
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import PetsList from '../components/PetsList';
+import NewPetModal from '../components/NewPetModal';
+import Loader from '../components/Loader';
 
-
-export default function Pets () {
-  const [modal, setModal] = useState(false)
-
-
-  const onSubmit = input => {
-    setModal(false)
+// Fragments
+const PETS_FIELDS = gql`
+  fragment PetsFields on Pet {
+    id
+    name
+    img
+    type
+    vaccinated @client
+    owner {
+      id
+      age @client
+    }
   }
-  
+`;
+
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`;
+
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`;
+
+export default function Pets() {
+  const [modal, setModal] = useState(false);
+  const { data, loading, error } = useQuery(ALL_PETS);
+  const [createPet, newPet] = useMutation(NEW_PET, {
+    update(cache, { data: { addPet } }) {
+      const allPets = cache.readQuery({ query: ALL_PETS });
+      cache.writeQuery({
+        query: ALL_PETS,
+        data: { pets: [addPet, ...allPets.pets] },
+      });
+    },
+  });
+
+  const onSubmit = (input) => {
+    console.log(input);
+    setModal(false);
+
+    createPet({
+      variables: { newPet: input },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        addPet: {
+          __typename: 'Pet',
+          id: Math.floor(Math.random() * 1000 + ''),
+          name: input.name,
+          type: input.type,
+          img: 'https://via.placeholder.com/300',
+          owner: {
+            __typename:'User',
+            id: 'ayhaga',
+            age: 32,
+          },
+        },
+      },
+    });
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error || newPet.error) {
+    return <p>error</p>;
+  }
   if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
   }
 
   return (
-    <div className="page pets-page">
+    <div className='page pets-page'>
       <section>
-        <div className="row betwee-xs middle-xs">
-          <div className="col-xs-10">
+        <div className='row betwee-xs middle-xs'>
+          <div className='col-xs-10'>
             <h1>Pets</h1>
           </div>
 
-          <div className="col-xs-2">
+          <div className='col-xs-2'>
             <button onClick={() => setModal(true)}>new pet</button>
           </div>
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
-  )
+  );
 }
+
+// Pets.fragments ={
+//   petsFields:gql``
+// }
